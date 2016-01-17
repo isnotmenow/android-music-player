8294fdd7dc6a0d1f9fca3c850cbe7ca3f11f3c17
l0stfake7 <bartek.poniatyszyn@gmail.com>
2016-01-17 21:13:00 +0100

    Update 1.0
    Fix repository

---
+++ app/src/main/java/pl/newstech/musicplayer/MusicController.java
@@ -0,0 +1,24 @@
+package pl.newstech.musicplayer;
+
+/**
+ * Created by Bartek on 20.10.2015.
+ */
+import android.content.Context;
+import android.widget.MediaController;
+
+/*
+ * This is demo code to accompany the Mobiletuts+ series:
+ * Android SDK: Creating a Music Player
+ *
+ * Sue Smith - February 2014
+ */
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
